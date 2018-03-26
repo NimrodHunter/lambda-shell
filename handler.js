4092fb0a5b96942eda24c987ae5bf2f19fb05a2d
Anibal Eduardo <anibal.catalan@consensys.net>
2018-03-26 10:46:33 -0400

    add babel plugin and async await functions

--- handler.js
+++ handler.js
@@ -2,13 +2,15 @@
 
 const providers = require('ethers').providers;
 
-module.exports.provider = (event, context, callback) => {
+module.exports.provider = async (event, context, callback) => {
   const provider = new providers.InfuraProvider(process.env.BN_PROVIDER_NAME, process.env.BN_PROVIDER_TOKEN);
+  const nonce = await provider.getTransactionCount('0xa5652e6244F673cc20c9fb6BeD1572F62183aC8e');
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'I can user ethers!',
-      provider: provider
+      provider: provider,
+      nonce: nonce
     }),
   };
 
